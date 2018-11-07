31486caa023860b01943613f95907822f33fe6b9
CFHH <34704169+CFHH@users.noreply.github.com>
2018-11-07 11:15:27 +0800

    warning C4146: 一元负运算符应用于无符号类型，结果仍为无符号类型

--- arith/montfp.c
+++ arith/montfp.c
@@ -615,6 +615,10 @@ void field_init_mont_fp(field_ptr f, mpz_t prime) {
   // since we're only doing it once.
   mpz_setbit(z, p->bytes * 8);
   mpz_invert(z, prime, z);
+#ifdef _MSC_VER
+  p->negpinv = -(mp_limb_signed_t)mpz_get_ui(z);
+#else
   p->negpinv = -mpz_get_ui(z);
+#endif
   mpz_clear(z);
 }
